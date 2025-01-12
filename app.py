b835dbf731b2ebb6131b703c52b6875bb1309d06
Priyanshu kumar <priyanshu12656@gmail.com>
2025-01-12 15:22:38 +0530

    Configured Git for line endings handling

--- app.py
+++ app.py
@@ -1,60 +1,61 @@
-import streamlit as st
+from flask import Flask, render_template, request
 import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt_tab')
 
+# Initialize Flask app
+app = Flask(__name__)
+
+# Download necessary NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Initialize the PorterStemmer
 ps = PorterStemmer()
 
+# Load the pre-trained model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
+# Function to preprocess the input text
 def transform_text(text):
-    text = text.lower()  ## this method change uppercase Into lowercase
-    text = nltk.word_tokenize(text)  ## this method convert text into single words
+    text = text.lower()  # Convert to lowercase
+    text = nltk.word_tokenize(text)  # Tokenize the text into words
 
     y = []
     for i in text:
-        if i.isalnum():  ## is alpha numeric   # Removing special character
+        if i.isalnum():  # Remove special characters
             y.append(i)
-
-    text = y[:]  ## here we empty our list
-    y.clear()  ## clear the y
-
-    for i in text:
-        if i not in stopwords.words(
-                'english') and i not in string.punctuation:  ## here we checking Stopwords and punctuation means all symbol,maths,brackets,quotation ('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
-            y.append(i)  ## return fresh Y
 
     text = y[:]
     y.clear()
 
     for i in text:
-        y.append(ps.stem(i))  ##Steaming words into small words like running -> run
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)  # Remove stopwords and punctuation
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))  # Apply stemming
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+# Define route for the home page
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+    if request.method == "POST":
+        input_sms = request.form['sms']
+        transform_sms = transform_text(input_sms)
+        vector_input = tfidf.transform([transform_sms])
+        prediction = model.predict(vector_input)[0]
+        result = "Spam" if prediction == 1 else "Not Spam"
+    return render_template("home.html", result=result)
 
-st.title('Email/sms Spam Classifier')
-st.text('By Priyanshu')
-
-input_sms = st.text_area("enter the message")
-if st.button('Predict'):
-    # 1.Preprocess
-    transform_sms = transform_text(input_sms)
-    # 2.vectorize
-    vector_input = tfidf.transform([transform_sms])
-    # 3.predict
-    result = model.predict(vector_input)[0]
-    # 4.display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
-
-# For Running this code:
-#     streamlit run app.py
-
-
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
