3302df7ac95089b0eab50395ee24b5fce5214df7
Priyanshu kumar <priyanshu12656@gmail.com>
2025-01-12 15:30:29 +0530

    Commit it

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Initialize Flask app
 app = Flask(__name__)
 
 # Download necessary NLTK resources
@@ -14,10 +13,6 @@ nltk.download('stopwords')
 
 # Initialize the PorterStemmer
 ps = PorterStemmer()
-
-# Load the pre-trained model and vectorizer
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
 
 # Function to preprocess the input text
 def transform_text(text):
@@ -44,18 +39,26 @@ def transform_text(text):
 
     return " ".join(y)
 
-# Define route for the home page
-@app.route("/", methods=["GET", "POST"])
-def home():
-    result = None
-    if request.method == "POST":
-        input_sms = request.form['sms']
-        transform_sms = transform_text(input_sms)
-        vector_input = tfidf.transform([transform_sms])
-        prediction = model.predict(vector_input)[0]
-        result = "Spam" if prediction == 1 else "Not Spam"
-    return render_template("home.html", result=result)
+# Load the pre-trained model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
-# Run the app
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_sms = request.form['text']
+    transform_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transform_sms])
+    result = model.predict(vector_input)[0]
+    
+    if result == 1:
+        return {'result': 'Spam'}
+    else:
+        return {'result': 'Not Spam'}
+
 if __name__ == "__main__":
     app.run(debug=True)
+S
